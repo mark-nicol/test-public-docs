065b6b2e643c17043235b4f9171e24257dc013b8
mnicol <mark.nicol@stfc.ac.uk>
2022-12-20 16:26:38 +0000

    toc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,4 +29,3 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = 'sphinxawesome_theme'
-html_static_path = ['_static']
